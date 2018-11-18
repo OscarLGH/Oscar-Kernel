581d8d3ab10ff5dc98c473a009edd601e78162ed
OscarLGH <OscarLGH@live.com>
2018-11-19 01:46:49 +0800

    Fix inline assembly bugs in in_out.h

--- arch/x86_64/include/in_out.h
+++ arch/x86_64/include/in_out.h
@@ -3,51 +3,51 @@
 
 #include <types.h>
 
-static inline u8 in8(u16 port)
+static inline u8 in8(u64 port)
 {
 	u8 data;
 	asm volatile("inb %%dx, %%al\n\t"
-				: "=rax"(data) : "rdx"(port)
+				: "=a"(data) : "d"(port)
 		);
 	return data;
 }
 
-static inline u16 in16(u16 port)
+static inline u16 in16(u64 port)
 {
 	u16 data;
 	asm volatile("inw %%dx, %%ax\n\t"
-				: "=rax"(data) : "rdx"(port)
+				: "=a"(data) : "d"(port)
 		);
 	return data;
 }
 
-static inline u32 in32(u16 port)
+static inline u32 in32(u64 port)
 {
 	u32 data;
 	asm volatile("inl %%dx, %%eax\n\t"
-				: "=rax"(data) : "rdx"(port)
+				: "=a"(data) : "d"(port)
 		);
 	return data;
 }
 
-static inline void out8(u16 port, u8 data)
+static inline void out8(u64 port, u8 data)
 {
 	asm volatile("outb %%al, %%dx\n\t"
-				:: "al"(data), "dx"(port)
+				:: "a"(data), "d"(port)
 		);
 }
 
-static inline void out16(u16 port, u16 data)
+static inline void out16(u64 port, u16 data)
 {
 	asm volatile("outw %%ax, %%dx\n\t"
-				:: "ax"(data), "dx"(port)
+				:: "a"(data), "d"(port)
 		);
 }
 
-static inline void out32(u16 port, u32 data)
+static inline void out32(u64 port, u32 data)
 {
 	asm volatile("outl %%eax, %%dx\n\t"
-				:: "eax"(data), "dx"(port)
+				:: "a"(data), "d"(port)
 		);
 }
 
