d12129caa0b756a6f5f9acd92f77c675edc9e719
liguanhua <ligh429@tsinghua.edu.cn>
2018-11-27 23:59:58 +0800

    Add cpu management struct. Add irq management struct.

--- arch/x86_64/include/arch.h
+++ arch/x86_64/include/arch.h
@@ -3,9 +3,19 @@
 
 #include <types.h>
 #include <cpuid.h>
+#include <paging.h>
+#include <segment.h>
 
 #define PERCPU_AREA_BASE 0x2000000
 #define PERCPU_AREA_SIZE 0x8000
+
+struct x86_cpu {
+	struct segment_desc *gdt_base;
+	struct gate_desc *idt_base;
+	struct tss_desc *tss;
+	u64 *pml4t_base;
+};
+
 
 inline static u64 get_percpu_area_base()
 {
