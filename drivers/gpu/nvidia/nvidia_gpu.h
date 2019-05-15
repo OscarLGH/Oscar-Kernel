b9a8c9bc489d2ef1484ee27817df741058a9bd0b
OscarLGH <OscarLGH@live.com>
2019-05-16 03:29:59 +0800

    nvidia gpu:add bar vm.

--- drivers/gpu/nvidia/nvidia_gpu.h
+++ drivers/gpu/nvidia/nvidia_gpu.h
@@ -4,6 +4,8 @@
 #include <init.h>
 #include <bitmap.h>
 
+struct nvidia_gpu;
+
 enum nvkm_memory_target {
 	NVKM_MEM_TARGET_INST, /* instance memory */
 	NVKM_MEM_TARGET_VRAM, /* video memory */
@@ -11,30 +13,107 @@ enum nvkm_memory_target {
 	NVKM_MEM_TARGET_NCOH, /* non-coherent system memory */
 };
 
+struct nv_inst_obj {
+	struct nvidia_gpu *gpu;
+	u64 inst_mem_addr;
+	u64 size;
+	u64 *mapped_ptr;
+};
+
 struct nvkm_memory {
 	struct list_head zone_list;
-	struct bitmap *allocate;
+	struct bitmap *alloc_bitmap;
 	u32 (*read) (u64 offset);
 	void (*write) (u64 offset, u32 value);
 };
 
+/* memory type/access flags, do not match hardware values */
+#define NV_MEM_ACCESS_RO  1
+#define NV_MEM_ACCESS_WO  2
+#define NV_MEM_ACCESS_RW (NV_MEM_ACCESS_RO | NV_MEM_ACCESS_WO)
+#define NV_MEM_ACCESS_SYS 4
+#define NV_MEM_ACCESS_VM  8
+#define NV_MEM_ACCESS_NOSNOOP 16
+
+#define NV_MEM_LARGE_PAGE 32
+
+#define NV_MEM_TARGET_VRAM        (0 << 8)
+#define NV_MEM_TARGET_HOST         (1 << 8)
+#define NV_MEM_TARGET_HOST_NOSNOOP (2 << 8)
+#define NV_MEM_TARGET_VM          (3 << 8)
+#define NV_MEM_TARGET_GART        (4 << 8)
+
+//NV PDE attributes
+#define NV_LPT_PRESENT BIT0
+#define NV_PT_SIZE_FULL  0
+#define NV_PT_SIZE_64M   (1 << 2)
+#define NV_PT_SIZE_32M	 (2 << 2)
+#define NV_PT_SIZE_16M   (3 << 3)
+#define NV_SPT_PRESENT BIT0
+
+//NV PTE attributes
+#define NV_PTE_PRESENT BIT0
+#define NV_PTE_SUPERVISOR_ONLY BIT1
+#define NV_PTE_READ_ONLY BIT2
+#define NV_PTE_ENCRYPTED BIT3  //SYSRAM only
+#define NV_PTE_TARGET_VRAM (0ULL << 32)
+#define NV_PTE_TARGET_SYSRAM_SNOOP (5ULL << 32)
+#define NV_PTE_TARGET_SYSRAM_NOSNOOP (7ULL << 32)
+
+#define NV_PAGE_SIZE_128KB		0
+#define NV_PAGE_SIZE_4KB		1
+
+#define NVC0_SPAGE_SHIFT        12
+#define NVC0_LPAGE_SHIFT        17
+#define NVC0_SPAGE_MASK         0x00fff
+#define NVC0_LPAGE_MASK         0x1ffff
+
+#define NVC0_VM_PDE_COUNT       0x2000
+#define NVC0_VM_BLOCK_SIZE      0x8000000
+#define NVC0_VM_BLOCK_MASK      0x7ffffff
+#define NVC0_VM_SPTE_COUNT      (NVC0_VM_BLOCK_SIZE >> NVC0_SPAGE_SHIFT)
+#define NVC0_VM_LPTE_COUNT      (NVC0_VM_BLOCK_SIZE >> NVC0_LPAGE_SHIFT)
+
+#define NVC0_PDE(a)             ((a) / NVC0_VM_BLOCK_SIZE)
+#define NVC0_SPTE(a)            (((a) & NVC0_VM_BLOCK_MASK) >> NVC0_SPAGE_SHIFT)
+#define NVC0_LPTE(a)            (((a) & NVC0_VM_BLOCK_MASK) >> NVC0_LPAGE_SHIFT)
+
+#define NVC0_PDE_HT_SIZE 32
+#define NVC0_PDE_HASH(n) (n % NVC0_PDE_HT_SIZE)
+
+#define PDE_2_ADDR(a) (((a) & 0xfffffffe) << 8)
+
 struct nvkm_mmu {
-	int (*map) (u64 virt, u64 phys, u64 len);
-	int (*unmap) (u64 virt, u64 phys, u64 len);
-	int (*mmu_walk) (u64 virt, u64 *pt);
+	int (*map_page) (struct nv_inst_obj *pgd, u64 virt, u64 phys, u64 page_size);
+	int (*unmap) (struct nv_inst_obj *pgd, u64 virt, u64 phys, u64 len);
+	int (*flush) ();
+	int (*mmu_walk) (struct nv_inst_obj *pgd, u64 virt, u64 *pt);
 };
 
 struct gp_fifo {
+	struct nv_inst_obj *ramfc;
+	struct nv_inst_obj *pgd;
 	
 };
 
-struct nvidia_gpu;
+
 struct nv_chipset {
 	u64 chipset;
 	int (*bios_init)(struct nvidia_gpu *gpu);
 	u64 (*vram_probe)(struct nvidia_gpu *gpu);
 };
 
+struct fb {
+	struct nv_inst_obj *fb;
+};
+
+struct bar_vm {
+	u64 bar_no;
+	u64 bar_base;
+	u64 bar_size;
+	struct nv_inst_obj *inst_mem;
+	struct nv_inst_obj *pgd;
+};
 
 struct nvidia_gpu {
 	struct pci_dev *pdev;
@@ -44,6 +123,8 @@ struct nvidia_gpu {
 
 	struct nvkm_memory memory;
 	struct nvkm_mmu mmu;
+	struct fb fb;
+	struct bar_vm bar[2];
 };
 
 static inline u32 nvkm_rd32(struct nvidia_gpu *gpu, u64 offset)
@@ -79,6 +160,63 @@ static inline void instmem_wr32(struct nvidia_gpu *gpu, u64 offset, u32 data)
 
 	nvkm_wr32(gpu, 0x001700, base >> 16);
 	nvkm_wr32(gpu, 0x700000 + addr, data);
+}
+
+static inline u64 instmem_allocate(struct nvidia_gpu *gpu, u64 len)
+{
+	u32 pages_allocate = len > 0x1000 ? len / 0x1000 : 1;
+	int ret = bitmap_allocate_bits(gpu->memory.alloc_bitmap, pages_allocate);
+	if (ret == -1) {
+		printk("instmem allocate failed.\n");
+		return -1;
+	}
+
+	return ret * 0x1000;
+}
+
+static inline void instmem_free(struct nvidia_gpu *gpu, u64 offset)
+{
+	//TODO
+}
+
+static inline struct nv_inst_obj *nv_instobj_alloc(struct nvidia_gpu *gpu, u64 size)
+{
+	u64 addr = instmem_allocate(gpu, size);
+	struct nv_inst_obj *obj;
+	if (addr == -1) {
+		return NULL;
+	}
+	obj = kmalloc(sizeof(*obj), GFP_KERNEL);
+	obj->gpu = gpu;
+	obj->inst_mem_addr = addr;
+	obj->size = size;
+	obj->mapped_ptr = NULL;
+
+	return obj;
+}
+
+static inline void nv_instobj_free(struct nv_inst_obj *obj)
+{
+	instmem_free(obj->gpu, obj->inst_mem_addr);
+	kfree(obj);
+}
+
+static inline int nv_instobj_rd32(struct nv_inst_obj *obj, u64 offset, u32 *value)
+{
+	if (offset > obj->inst_mem_addr + obj->size) {
+		return -1;
+	}
+	*value = instmem_rd32(obj->gpu, obj->inst_mem_addr + offset);
+	return 0;
+}
+
+static inline int nv_instobj_wr32(struct nv_inst_obj *obj, u64 offset, u32 value)
+{
+	if (offset > obj->inst_mem_addr + obj->size) {
+		return -1;
+	}
+	instmem_wr32(obj->gpu, obj->inst_mem_addr + offset, value);
+	return 0;
 }
 
 
--- drivers/gpu/nvidia/nvidia_gpu_drv.c
+++ drivers/gpu/nvidia/nvidia_gpu_drv.c
@@ -2,26 +2,91 @@
 
 u64 vram_probe_gm107(struct nvidia_gpu *gpu)
 {
-	u32 fbpt = nvkm_rd32(gpu, 0x022438);
+	u32 fbps = nvkm_rd32(gpu, 0x022438);
 	u32 fbpat = nvkm_rd32(gpu, 0x02243c);
-	u32 fbpas = fbpat / fbpt;
+	u32 fbpas = fbpat / fbps;
 	u32 fbpao = nvkm_rd32(gpu, 0x021c14);
+	u32 fbp;
 	u32 fbpa = 1 * fbpas;
 	u32 size = 0;
 
-	while (fbpas--) {
-		if (!(fbpao & (1 << fbpa)))
-			size += nvkm_rd32(gpu, 0x011020c + (fbpa * 0x1000));
-			printk("size = %x\n", size);
-		fbpa++;
+	for (fbp = 0; fbp < fbps; fbp++) {
+		while (fbpas--) {
+			if (!(fbpao & (1 << fbpa)))
+				size += nvkm_rd32(gpu, 0x011020c + (fbpa * 0x1000));
+			fbpa++;
+		}
 	}
-	printk("VRAM:%d MB.\n", size >> 20);
+	printk("VRAM:%d MB.\n", size);
+	return size << 20;
 }
+
+u64 vram_probe_gp100(struct nvidia_gpu *gpu)
+{
+	u32 fbps = nvkm_rd32(gpu, 0x022438);
+	u32 ltcs = nvkm_rd32(gpu, 0x022450);
+	u32 fbpao = nvkm_rd32(gpu, 0x021c14);
+	u32 fbp;
+	u32 fbpas;
+
+	u32 fbpa = fbp * fbpas;
+	u64 size = 0;
+	
+	for (fbp = 0; fbp < fbps; fbp++) {
+		if (!(nvkm_rd32(gpu, 0x021d38) & (1 << fbp))) {
+			//u32 ltco = nvkm_rd32(gpu, 0x021d70 + (fbp * 4));
+			//u32 ltcm = ~ltco & ((1 << ltcs) - 1);
+			fbpas = nvkm_rd32(gpu, 0x022458);
+			while (fbpas--) {
+				if (!(fbpao & (1 << fbpa)))
+					size += nvkm_rd32(gpu, 0x90020c + (fbpa * 0x4000));
+				fbpa++;
+			}
+		}
+	}
+	printk("VRAM:%d MB.\n", size);
+	return size << 20;
+}
+
+
+int nvidia_tlb_flush(struct nvidia_gpu *gpu, u64 pt_addr)
+{
+	nvkm_wr32(gpu, 0x100cb8, pt_addr >> 8);
+	nvkm_wr32(gpu, 0x100cbc, 0x80000000 | 0x1);
+}
+
+int nvidia_mmu_map_page(struct nv_inst_obj *pgd, u64 virt, u64 phys, u64 priv, u64 type, u64 page_size)
+{
+	int ret;
+	struct nvidia_gpu *gpu = pgd->gpu;
+	u64 pd_index = NVC0_PDE(virt);
+	u32 pde0;
+	u32 pde1;
+	struct nv_inst_obj *pt;
+	u64 pt_index = NVC0_SPTE(virt);
+	u32 pte0 = (phys >> 8) | priv | NV_PTE_PRESENT;
+	u32 pte1 = type;
+	ret = nv_instobj_rd32(pgd, pd_index * 8 + 0, &pde0);
+	ret = nv_instobj_rd32(pgd, pd_index * 8 + 4, &pde1);
+	if (pde1 == 0) {
+		pt = nv_instobj_alloc(gpu, 0x8000);
+		nv_instobj_wr32(pgd, pd_index * 8 + 4, (pt->inst_mem_addr >> 8) | NV_SPT_PRESENT);
+		nv_instobj_rd32(pgd, pd_index * 8 + 4, &pde1);
+	}
+
+	//printk("pde1 = %x pte = %x\n", pde1, pte0);
+	instmem_wr32(gpu, PDE_2_ADDR(pde1) + pt_index * 8, pte0);
+	instmem_wr32(gpu, PDE_2_ADDR(pde1) + pt_index * 8 + 4, pte1);
+
+	nvidia_tlb_flush(gpu, pde1 + pt_index * 8);
+	return 0;
+}
+
 
 struct nv_chipset nv_chipsets[] = {
 	{0x118, 0, vram_probe_gm107},
-	{0x132, 0, 0},
-	{0x134, 0, 0}
+	{0x132, 0, vram_probe_gp100},
+	{0x134, 0, vram_probe_gp100}
 };
 
 int nvidia_gpu_chip_probe(struct nvidia_gpu *gpu)
@@ -41,19 +106,45 @@ int nvidia_gpu_memory_init(struct nvidia_gpu *gpu)
 {
 	int i;
 	u64 ram_size = gpu->chipset->vram_probe(gpu);
-	gpu->memory.allocate = bitmap_alloc(ram_size / 0x1000);
+	gpu->memory.alloc_bitmap = bitmap_alloc(ram_size / 0x1000);
+
+	gpu->fb.fb = nv_instobj_alloc(gpu, 0x1000000);
+
+	/*
+	for (i = 0; i < 0x1000000; i += 4) {
+		nv_instobj_wr32(gpu->fb.fb, i, 0xf00ff0);
+	}
+	*/
+	
+	
 }
 
 int nvidia_gpu_bar_init(struct nvidia_gpu *gpu)
 {
-	u64 bar1_base, bar1_size, bar3_base, bar3_size;
+	int i;
 	struct pci_dev *pdev = gpu->pdev;
-	bar1_base = pci_get_bar_base(pdev, 1);
-	bar1_size = pci_get_bar_size(pdev, 1);
-	bar3_base = pci_get_bar_base(pdev, 3);
-	bar3_size = pci_get_bar_size(pdev, 3);
-	printk("bar1 base:0x%x, size = 0x%x\n", bar1_base, bar1_size);
-	printk("bar3 base:0x%x, size = 0x%x\n", bar3_base, bar3_size);
+	gpu->bar[0].bar_base = pci_get_bar_base(pdev, 1);
+	gpu->bar[0].bar_size = pci_get_bar_size(pdev, 1);
+	gpu->bar[1].bar_base = pci_get_bar_base(pdev, 3);
+	gpu->bar[1].bar_size = pci_get_bar_size(pdev, 3);
+	printk("bar1 base:0x%x, size = 0x%x\n", gpu->bar[0].bar_base, gpu->bar[0].bar_size);
+	printk("bar3 base:0x%x, size = 0x%x\n", gpu->bar[1].bar_base, gpu->bar[1].bar_size);
+	gpu->bar[0].inst_mem = nv_instobj_alloc(gpu, 0x1000);
+	gpu->bar[0].pgd = nv_instobj_alloc(gpu, 0x2000 * 8);
+	for (i = 0; i < 0x2000 * 8; i += 4)
+		nv_instobj_wr32(gpu->bar[0].pgd, i, 0);
+
+	nv_instobj_wr32(gpu->bar[0].inst_mem, 0x200, gpu->bar[0].pgd->inst_mem_addr);
+	nv_instobj_wr32(gpu->bar[0].inst_mem, 0x204, gpu->bar[0].pgd->inst_mem_addr >> 32);
+	nv_instobj_wr32(gpu->bar[0].inst_mem, 0x208, gpu->bar[0].bar_size - 1);
+	nv_instobj_wr32(gpu->bar[0].inst_mem, 0x20c, (gpu->bar[0].bar_size - 1) >> 32);
+
+	for (i = 0; i < 0x4000000; i += 0x1000)
+		nvidia_mmu_map_page(gpu->bar[0].pgd, i, i, 0, 0, 0x1000);
+
+	printk("switch to vmm bar...\n");
+
+	nvkm_wr32(gpu, 0x001704, 0x80000000 | (gpu->bar[0].inst_mem->inst_mem_addr >> 12));
 }
 
 int nvidia_gpu_probe(struct pci_dev *pdev, struct pci_device_id *pent)
@@ -83,7 +174,9 @@ void nvidia_gpu_remove(struct pci_dev *pdev)
 
 
 struct pci_device_id nvidia_ids[64] = {
-	{0x134f10de, 0x030200, 0xffffffff, 0x0}
+	{0x134f10de, 0x030200, 0xffffffff, 0x0},
+	{0x1b8010de, 0x030200, 0xffffffff, 0x0},
+	{0x1b0610de, 0x030200, 0xffffffff, 0x0}
 };
 
 struct pci_driver nvidia_gpu_driver = {
