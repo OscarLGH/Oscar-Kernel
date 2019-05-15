40835614b97b30e6e662d55ed90202e62b59d0ea
liguanhua <ligh429@tsinghua.edu.cn>
2019-05-15 21:04:43 +0800

    Add some gpu struct.

--- drivers/gpu/nvidia/nvidia_gpu.h
+++ drivers/gpu/nvidia/nvidia_gpu.h
@@ -2,8 +2,18 @@
 #include <kernel.h>
 #include <mm.h>
 #include <init.h>
+#include <bitmap.h>
+
+enum nvkm_memory_target {
+	NVKM_MEM_TARGET_INST, /* instance memory */
+	NVKM_MEM_TARGET_VRAM, /* video memory */
+	NVKM_MEM_TARGET_HOST, /* coherent system memory */
+	NVKM_MEM_TARGET_NCOH, /* non-coherent system memory */
+};
 
 struct nvkm_memory {
+	struct list_head zone_list;
+	struct bitmap *allocate;
 	u32 (*read) (u64 offset);
 	void (*write) (u64 offset, u32 value);
 };
@@ -18,25 +28,57 @@ struct gp_fifo {
 	
 };
 
+struct nvidia_gpu;
+struct nv_chipset {
+	u64 chipset;
+	int (*bios_init)(struct nvidia_gpu *gpu);
+	u64 (*vram_probe)(struct nvidia_gpu *gpu);
+};
+
 
 struct nvidia_gpu {
 	struct pci_dev *pdev;
 	u64 mmio_base;
 	volatile u32 *mmio_virt;
-	u32 chipset;
+	struct nv_chipset *chipset;
 
 	struct nvkm_memory memory;
 	struct nvkm_mmu mmu;
 };
 
-static inline u32 nvkm_rd(struct nvidia_gpu *gpu, u64 offset)
+static inline u32 nvkm_rd32(struct nvidia_gpu *gpu, u64 offset)
 {
 	return gpu->mmio_virt[offset / 4];
 }
 
-static inline void nvkm_wr(struct nvidia_gpu *gpu, u64 offset, u32 value)
+static inline void nvkm_wr32(struct nvidia_gpu *gpu, u64 offset, u32 value)
 {
 	gpu->mmio_virt[offset / 4] = value;
+}
+
+static inline u32 instmem_rd32(struct nvidia_gpu *gpu, u64 offset)
+{
+	u64 base = offset & 0xffffff00000ULL;
+	u64 addr = offset & 0x000000fffffULL;
+	u32 data;
+
+	//TODO:add spinlock here.
+
+	nvkm_wr32(gpu, 0x001700, base >> 16);
+	data = nvkm_rd32(gpu, 0x700000 + addr);
+
+	return data;
+}
+
+static inline void instmem_wr32(struct nvidia_gpu *gpu, u64 offset, u32 data)
+{
+	u64 base = offset & 0xffffff00000ULL;
+	u64 addr = offset & 0x000000fffffULL;
+
+	//TODO:add spinlock here.
+
+	nvkm_wr32(gpu, 0x001700, base >> 16);
+	nvkm_wr32(gpu, 0x700000 + addr, data);
 }
 
 
--- drivers/gpu/nvidia/nvidia_gpu_drv.c
+++ drivers/gpu/nvidia/nvidia_gpu_drv.c
@@ -1,5 +1,61 @@
 #include "nvidia_gpu.h"
 
+u64 vram_probe_gm107(struct nvidia_gpu *gpu)
+{
+	u32 fbpt = nvkm_rd32(gpu, 0x022438);
+	u32 fbpat = nvkm_rd32(gpu, 0x02243c);
+	u32 fbpas = fbpat / fbpt;
+	u32 fbpao = nvkm_rd32(gpu, 0x021c14);
+	u32 fbpa = 1 * fbpas;
+	u32 size = 0;
+
+	while (fbpas--) {
+		if (!(fbpao & (1 << fbpa)))
+			size += nvkm_rd32(gpu, 0x011020c + (fbpa * 0x1000));
+			printk("size = %x\n", size);
+		fbpa++;
+	}
+	printk("VRAM:%d MB.\n", size >> 20);
+}
+
+struct nv_chipset nv_chipsets[] = {
+	{0x118, 0, vram_probe_gm107},
+	{0x132, 0, 0},
+	{0x134, 0, 0}
+};
+
+int nvidia_gpu_chip_probe(struct nvidia_gpu *gpu)
+{
+	int i;
+	u32 chipset = (nvkm_rd32(gpu, 0) >> 20) & 0x1ff;
+	printk("nvidia chipset = %x\n", chipset);
+	for (i = 0; i < sizeof(nv_chipsets) / sizeof(struct nv_chipset); i++) {
+		if (nv_chipsets[i].chipset == chipset) {
+			gpu->chipset = &nv_chipsets[i];
+			break;
+		}
+	}
+}
+
+int nvidia_gpu_memory_init(struct nvidia_gpu *gpu)
+{
+	int i;
+	u64 ram_size = gpu->chipset->vram_probe(gpu);
+	gpu->memory.allocate = bitmap_alloc(ram_size / 0x1000);
+}
+
+int nvidia_gpu_bar_init(struct nvidia_gpu *gpu)
+{
+	u64 bar1_base, bar1_size, bar3_base, bar3_size;
+	struct pci_dev *pdev = gpu->pdev;
+	bar1_base = pci_get_bar_base(pdev, 1);
+	bar1_size = pci_get_bar_size(pdev, 1);
+	bar3_base = pci_get_bar_base(pdev, 3);
+	bar3_size = pci_get_bar_size(pdev, 3);
+	printk("bar1 base:0x%x, size = 0x%x\n", bar1_base, bar1_size);
+	printk("bar3 base:0x%x, size = 0x%x\n", bar3_base, bar3_size);
+}
+
 int nvidia_gpu_probe(struct pci_dev *pdev, struct pci_device_id *pent)
 {
 	u16 pci_command_reg;
@@ -7,15 +63,17 @@ int nvidia_gpu_probe(struct pci_dev *pdev, struct pci_device_id *pent)
 	u32 *mmio_virt;
 	u32 boot0, strap;
 	struct nvidia_gpu *gpu = kmalloc(sizeof(*gpu), GFP_KERNEL);
-	printk("%s\n", __FUNCTION__);
+	gpu->pdev = pdev;
 	pci_read_config_word(pdev, PCI_COMMAND, &pci_command_reg);
 	pci_command_reg |= (PCI_COMMAND_MEMORY | PCI_COMMAND_MASTER);
 	pci_write_config_word(pdev, PCI_COMMAND, pci_command_reg);
 	gpu->mmio_virt = ioremap(pci_get_bar_base(pdev, 0), pci_get_bar_size(pdev, 0));
-	gpu->chipset = (nvkm_rd(gpu, 0) >> 20) & 0x1ff;
-	printk("nvidia chipset = %x\n", gpu->chipset);
 
 	pdev->private_data = gpu;
+
+	nvidia_gpu_chip_probe(gpu);
+	nvidia_gpu_memory_init(gpu);
+	nvidia_gpu_bar_init(gpu);
 }
 
 void nvidia_gpu_remove(struct pci_dev *pdev)
